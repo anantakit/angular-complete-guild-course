9a72edde63a4b127dc5b2f132bd257a80ba77fda
anantakit <frame.zz@hotmail.com>
2020-03-07 17:05:53 +0700

    learn ngfor ngclass ngstyle

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
+
   // template: `
   //   <app-server></app-server>
   //   <app-server></app-server>`,
@@ -14,21 +15,24 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
   constructor() {
     setTimeout(() => {
-      this.allowNewServer = true
-    },2000)
-   }
-
-  ngOnInit() {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
-  onCreateServer(){
-    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
 
-  onUpdateServerName(event: Event){
-    this.serverName = (<HTMLInputElement>event.target).value
+  onUpdateServerName(event: Event) {
+    this.serverName = ( event.target as HTMLInputElement).value;
   }
-
 }
